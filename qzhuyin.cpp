2dbcd7e09ce7c07ef6b6437d3439edfdaf0d4b3d
Simon Yuan <iyua002@aucklanduni.ac.nz>
2016-11-20 20:34:30 +1300

    Add commandline argument handling

--- qzhuyin.cpp
+++ qzhuyin.cpp
@@ -1,7 +1,10 @@
 #include <QDebug>
-#include <QApplication>
+#include <QGuiApplication>
+#include <QCommandLineParser>
 #include <QPdfWriter>
 #include <QScopedPointer>
+
+#include <errno.h>
 
 #include "ast.h"
 #include "astprinter.h"
@@ -12,6 +15,11 @@
 #include "pagerenderer.h"
 #include "qzy2ast.h"
 #include "zhchar.h"
+
+struct Options {
+    bool debug = false;
+    QString source;
+};
 
 AstNode* testParser(const QString &fileName)
 {
@@ -44,11 +52,38 @@ void testPageBuilder(AstNode *pRootNode)
     } while (xPage);
 }
 
+void parseOptions(const QCoreApplication &app, Options &options)
+{
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Chinese typsetting tool");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("source", QCoreApplication::translate("main", "Source file."));
+    parser.addOptions({
+        {{"d", "debug"},
+            QCoreApplication::translate("main", "Debug output.")},
+    });
+    parser.process(app);
+
+    options.debug = parser.isSet("debug");
+    QStringList positionalArguments = parser.positionalArguments();
+    if (positionalArguments.isEmpty())
+        parser.showHelp(EINVAL);
+    else
+        options.source = positionalArguments.first();
+}
+
 int main(int argc, char *argv[])
 {
-    QApplication app(argc,argv);
+    QGuiApplication app(argc, argv);
+    QCoreApplication::setApplicationName("qzhuyin");
+    QCoreApplication::setApplicationVersion("0.9");
+
+    Options options;
+    parseOptions(app, options);
+
     QScopedPointer<AstNode> xRootNode;
-    xRootNode.reset(testParser("example.qzy"));
+    xRootNode.reset(testParser(options.source));
     testPageBuilder(xRootNode.data());
     return 0;
 }
